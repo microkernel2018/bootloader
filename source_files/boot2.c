d0fecb64c268a89f0626fc76156c1e8835a5a8c6
DARK-KNIGHT-491 <rakeshlal.491@gmail.com>
2018-09-29 18:52:12 +0530

    Add files via upload

---
+++ source_files/boot2.c
@@ -0,0 +1,25 @@
+__asm__("cli\n");
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
+static void print_string(char* string){
+  volatile unsigned char* vid_mem = (unsigned char*) 0xb8000;
+  int j=0;
+  while(string[j]!='\0'){
+
+    *vid_mem++ = (unsigned char) string[j++];
+    *vid_mem++ = 0x09;
+  }
+}
+
+
+void boot_main(void){
+
+  char string[] = "Now Entered The Second Stage Program!";
+  print_string(string);
+
+
+for(;;);
+
+}
