b09cbaf18aebce0ee968142fc5685b4bdc59ffb4
rakesh491 <rakeshlal.491@gmail.com>
2018-09-29 23:12:56 +0530

    some more change

--- source_files/boot2.c
+++ source_files/boot2.c
@@ -2,21 +2,11 @@ __asm__("cli\n");
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
-
-static void print_string(char* string){
-  volatile unsigned char* vid_mem = (unsigned char*) 0xb8000;
-  int j=0;
-  while(string[j]!='\0'){
-
-    *vid_mem++ = (unsigned char) string[j++];
-    *vid_mem++ = 0x09;
-  }
-}
-
+#include <rak_print_string.h>
 
 void boot_main(void){
 
-  char string[] = "Now Entered The Second Stage Program!";
+  char string[] = "Entered the first stage of bootloader!";
   print_string(string);
 
 
